a749bc0c0670f31d4930d9258935dab37f3e89da
JuanCarlosHR2003 <105697745+JuanCarlosHR2003@users.noreply.github.com>
2024-03-16 15:31:34 -0600

    Challenge2_changes

--- MiniChallenge_2/reto_2/reto_2/signal_generator.py
+++ MiniChallenge_2/reto_2/reto_2/signal_generator.py
@@ -14,59 +14,97 @@ class My_Publisher(Node):
             namespace='',
             parameters=[
                 ('type_int', rclpy.Parameter.Type.INTEGER), 
-                ('time_float', rclpy.Parameter.Type.DOUBLE),
-                ('offset_float', rclpy.Parameter.Type.DOUBLE), 
-                ('frequency_float', rclpy.Parameter.Type.DOUBLE), 
-                ('amplitude_float', rclpy.Parameter.Type.DOUBLE)
+                ('sine.time', rclpy.Parameter.Type.DOUBLE),
+                ('sine.offset', rclpy.Parameter.Type.DOUBLE), 
+                ('sine.frequency', rclpy.Parameter.Type.DOUBLE), 
+                ('sine.amplitude', rclpy.Parameter.Type.DOUBLE),
+                ('sine.phase', rclpy.Parameter.Type.DOUBLE),
+                ('square.time', rclpy.Parameter.Type.DOUBLE),
+                ('square.offset', rclpy.Parameter.Type.DOUBLE), 
+                ('square.frequency', rclpy.Parameter.Type.DOUBLE), 
+                ('square.amplitude', rclpy.Parameter.Type.DOUBLE),
+                ('square.phase', rclpy.Parameter.Type.DOUBLE),
+                ('sawtooth.time', rclpy.Parameter.Type.DOUBLE),
+                ('sawtooth.offset', rclpy.Parameter.Type.DOUBLE), 
+                ('sawtooth.frequency', rclpy.Parameter.Type.DOUBLE), 
+                ('sawtooth.amplitude', rclpy.Parameter.Type.DOUBLE),
+                ('sawtooth.phase', rclpy.Parameter.Type.DOUBLE),
+                ('cosine.time', rclpy.Parameter.Type.DOUBLE),
+                ('cosine.offset', rclpy.Parameter.Type.DOUBLE), 
+                ('cosine.frequency', rclpy.Parameter.Type.DOUBLE), 
+                ('cosine.amplitude', rclpy.Parameter.Type.DOUBLE),
+                ('cosine.phase', rclpy.Parameter.Type.DOUBLE),
+                ('half_square.time', rclpy.Parameter.Type.DOUBLE),
+                ('half_square.offset', rclpy.Parameter.Type.DOUBLE), 
+                ('half_square.frequency', rclpy.Parameter.Type.DOUBLE), 
+                ('half_square.amplitude', rclpy.Parameter.Type.DOUBLE),
+                ('half_square.phase', rclpy.Parameter.Type.DOUBLE)
             ]
         )
         self.signal_publisher = self.create_publisher(Float32, 'signal', 10)
         self.signal_params_publisher = self.create_publisher(Signal, 'signal_params', 10)
-        signal_timer_period = 0.001
-        signal_params_timer_period = 0.1
-        self.signal_timer = self.create_timer(signal_timer_period, self.signal_timer_callback)
-        self.params_timer = self.create_timer(signal_params_timer_period, self.signal_params_timer_callback)
+        self.time = 0
+        self.signal_timer_period = 0.001
+        self.signal_params_timer_period = 0.1
+        self.signal_timer = self.create_timer(self.signal_timer_period, self.signal_timer_callback)
+        self.params_timer = self.create_timer(self.signal_params_timer_period, self.signal_params_timer_callback)
         self.get_logger().info('Signal generator node successfully initialized!!!')
         self.msg_signal = Float32()
         self.msg_param = Signal()
 
     def signal_timer_callback(self):
-        
-        type = self.get_parameter('type_int').get_parameter_value().integer_value
-        frequency = self.get_parameter('frequency_float').get_parameter_value().double_value
-        amplitude = self.get_parameter('amplitude_float').get_parameter_value().double_value
-        time = self.get_parameter('time_float').get_parameter_value().double_value
-        offset = self.get_parameter('offset_float').get_parameter_value().double_value
-        if(type == 1):
-            result = amplitude*np.sin(2*np.pi*time*frequency) + offset
-        elif(type == 2):
-            result = amplitude*signal.square(2*np.pi*time*frequency) + offset   
-        elif(type == 3):
-            result = amplitude*signal.sawtooth(2*np.pi*time*frequency) + offset
-        elif(type==4):
-            result = abs(amplitude*np.cos(2*np.pi*time*frequency)) + offset
-        elif(type==5):
-            result = -amplitude*signal.square(np.pi/2*time*frequency) + offset
+        self.time += self.signal_timer_period
+        self.type = self.get_parameter('type_int').get_parameter_value().integer_value
+        if(self.type == 1):
+            self.amplitude = self.get_parameter('sine.amplitude').get_parameter_value().double_value
+            self.frequency = self.get_parameter('sine.frequency').get_parameter_value().double_value
+            self.phase = self.get_parameter('sine.phase').get_parameter_value().double_value
+            self.offset = self.get_parameter('sine.offset').get_parameter_value().double_value
+            result = self.amplitude*np.sin(2*np.pi*self.time*self.frequency + self.phase) + self.offset
+        elif(self.type == 2):
+            self.amplitude = self.get_parameter('square.amplitude').get_parameter_value().double_value
+            self.frequency = self.get_parameter('square.frequency').get_parameter_value().double_value
+            self.phase = self.get_parameter('square.phase').get_parameter_value().double_value
+            self.offset = self.get_parameter('square.offset').get_parameter_value().double_value
+            result = self.amplitude*signal.square(2*np.pi*self.time*self.frequency + self.phase) + self.offset   
+        elif(self.type == 3):
+            self.amplitude = self.get_parameter('sawtooth.amplitude').get_parameter_value().double_value
+            self.frequency = self.get_parameter('sawtooth.frequency').get_parameter_value().double_value
+            self.phase = self.get_parameter('sawtooth.phase').get_parameter_value().double_value
+            self.offset = self.get_parameter('sawtooth.offset').get_parameter_value().double_value
+            result = self.amplitude*signal.sawtooth(2*np.pi*self.time*self.frequency + self.phase) + self.offset
+        elif(self.type==4):
+            self.amplitude = self.get_parameter('cosine.amplitude').get_parameter_value().double_value
+            self.frequency = self.get_parameter('cosine.frequency').get_parameter_value().double_value
+            self.phase = self.get_parameter('cosine.phase').get_parameter_value().double_value
+            self.offset = self.get_parameter('cosine.offset').get_parameter_value().double_value
+            result = abs(self.amplitude*np.cos(2*np.pi*self.time*self.frequency + self.phase)) + self.offset
+        elif(self.type==5):
+            self.amplitude = self.get_parameter('half_square.amplitude').get_parameter_value().double_value
+            self.requency = self.get_parameter('half_square.frequency').get_parameter_value().double_value
+            self.phase = self.get_parameter('half_square.phase').get_parameter_value().double_value
+            self.offset = self.get_parameter('half_square.offset').get_parameter_value().double_value
+            result = -self.amplitude*signal.square(np.pi/2*self.time*self.frequency + self.phase) + self.offset
 
         self.msg_signal.data = result
         self.signal_publisher.publish(self.msg_signal)
         self.get_logger().info('Signal: {}'.format(self.msg_signal.data))
-        param_time = Parameter('time_float', Parameter.Type.DOUBLE, time + 0.001)
-        self.set_parameters([param_time])
     
     def signal_params_timer_callback(self):
         
-        self.msg_param.type = self.get_parameter('type_int').get_parameter_value().integer_value
-        self.msg_param.frequency = self.get_parameter('frequency_float').get_parameter_value().double_value
-        self.msg_param.amplitude = self.get_parameter('amplitude_float').get_parameter_value().double_value
-        self.msg_param.time = self.get_parameter('time_float').get_parameter_value().double_value
-        self.msg_param.offset = self.get_parameter('offset_float').get_parameter_value().double_value
+        self.msg_param.type = self.type
+        self.msg_param.frequency = self.frequency
+        self.msg_param.amplitude = self.amplitude
+        self.msg_param.time = self.time
+        self.msg_param.offset = self.offset
+        self.msg_param.phase = self.phase
         self.signal_params_publisher.publish(self.msg_param)
         self.get_logger().info('Signal type: {}'.format(self.msg_param.type))
         self.get_logger().info('Signal frequency: {}'.format(self.msg_param.frequency))
         self.get_logger().info('Signal amplitude: {}'.format(self.msg_param.amplitude))
         self.get_logger().info('Signal time: {}'.format(self.msg_param.time))
         self.get_logger().info('Signal offset: {}'.format(self.msg_param.offset))
+        self.get_logger().info('Signal phase: {}'.format(self.msg_param.phase))
         
         
 def main(args=None):
